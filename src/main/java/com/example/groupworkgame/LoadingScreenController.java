60c99ba11de6410886d5dfa661109944ea832fc5
JackT-C <jt210471@reigate.ac.uk>
2022-03-30 12:37:34 +0100

    Scene Switch down got main menu to game done

--- src/main/java/com/example/groupworkgame/LoadingScreenController.java
+++ src/main/java/com/example/groupworkgame/LoadingScreenController.java
@@ -1,5 +1,6 @@
 package com.example.groupworkgame;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,6 +11,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Pair;
+
 import java.io.IOException;
 
 public class LoadingScreenController extends Application {
@@ -40,14 +43,52 @@ public class LoadingScreenController extends Application {
         Parent root = loader.load();
         Scene s2 = new Scene(root);
         Stage window = (Stage)(((Node)event.getSource()).getScene().getWindow());
-        window.setFullScreen(true);
-        window.setScene(s2);
-        window.show();
+        GameController controller = loader.getController();
+        Game.initialiseArrayLists(root, controller);
+        controller.initialiseObjects();
+        s2.setOnMousePressed(mouseEvent -> Game.player.setUp(mouseEvent.isPrimaryButtonDown()));
+        s2.setOnMouseReleased(mouseEvent -> Game.player.setUp(false));
+        //GAME LOOP
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (!Game.player.isDead) {
+                    Game.counter = updateCounter(Game.counter);
+                    updateEntities();
+                    if (controller.checkCollisions()) {
+                        System.out.println("you got: " + Game.score);
+                        Game.player.setDead(true);
+                    }
+                }
+            }
+            public int updateCounter(int counter){
+                Game.score++;
+                switch (counter) {
+                    case 25 -> controller.createTopObject();
+                    case 50 -> {
+                        controller.createBottomObject();
+                        counter = 0;
+                    }
+                }
+                counter++;
+                return counter;
+            }
+            public void updateEntities(){
+                controller.cycleBackground();
+                controller.moveObjects();
+                Pair<Float, Float> newPlayerPos = controller.updatePlayerPos(Game.player.isUp(), Game.player.getVelY(), Game.player.getRotY());
+                Game.player.setVelY(newPlayerPos.getKey());
+                Game.player.setRotY(newPlayerPos.getValue());
+            }
 
-}
+        };
+            timer.start();
+            window.setFullScreen(true);
+            window.setScene(s2);
+            window.show();
+
+    }
 //EXIT GAME
-    @FXML
-    private Button exitButton;
     @FXML
     private AnchorPane scenePane;
 
